b6172fbfef6464a49e033bca0600549516714d2f
root <root@refraktor.org>
2026-02-21 12:32:32 -0800

    - Updated code to use the new Matrix Authentication Service API. - Added function to map token to the internal ID for easier use

--- tokenbot.py
+++ tokenbot.py
@@ -1,5 +1,7 @@
-# token - A maubot plugin to manage your synapse registration tokens
-# Copyright (C) 2022 Michael Auer
+# mas-token - A maubot plugin to manage your synapse registration tokens via MAS
+# this is a fork of the maubot-token plugin by Michael Auer
+#
+# Copyright (C) 2026 Cassie Wintermute
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -27,7 +29,7 @@ error_msg_no_auth = "My mom said I'm not allowed to talk to strangers."
 token_msg = """
 **{}**\n
 - uses_allowed: {}\n
-- pending: {}\n
+- valid: {}\n
 - completed: {}\n
 - expiry_time: {}\n
 """
@@ -45,26 +47,29 @@ class Config(BaseProxyConfig):
 
 
 def parse_single_token(token):
-    expiry_time = token["expiry_time"]
-    if expiry_time:
-        expiry_time = datetime.datetime.fromtimestamp(expiry_time / 1000.0)
-    return token_msg.format(token["token"], token["uses_allowed"],
-                            token["pending"], token["completed"], expiry_time)
+    expiry_time = token["data"]["attributes"]["expires_at"]
+    #if expiry_time:
+    #    expiry_time = datetime.datetime.fromtimestamp(expiry_time / 1000.0)
+    return token_msg.format(token["data"]["attributes"]["token"], token["data"]["attributes"]["usage_limit"],
+                            token["data"]["attributes"]["valid"], token["data"]["attributes"]["expires_at"], expiry_time)
 
 
 def parse_tokens(json):
     valid_tokens = "\u2705 Valid Tokens \u2705\n"
     invalid_tokens = "\u274C Invalid Tokens \u274C\n"
-    for token in json["registration_tokens"]:
+    # To-Do: Rewrite to just check for token["attributes"]["valid"] lol
+    for token in json["data"]:
         valid = True
-        if token["uses_allowed"]:
-            valid = valid and token["completed"] < token["uses_allowed"]
-        if token["expiry_time"]:
-            valid = valid and token["expiry_time"] > int(time.time()) * 1000
+        if token["attributes"]["usage_limit"]:
+            valid = valid and token["attributes"]["times_used"] < token["attributes"]["usage_limit"]
+        if token["attributes"]["expires_at"]:
+            valid = valid and datetime.datetime.fromisoformat(token["attributes"]["expires_at"][:-1]) > datetime.datetime.now()
+        if token["attributes"]["revoked_at"]:
+            valid = False        
         if valid:
-            valid_tokens += "- {}\n".format(token["token"])
+            valid_tokens += "- {}\n".format(token["attributes"]["token"])
         else:
-            invalid_tokens += "- {}\n".format(token["token"])
+            invalid_tokens += "- {}\n".format(token["attributes"]["token"])
     return invalid_tokens + "\n" + valid_tokens
 
 
@@ -82,10 +87,35 @@ class TokenBot(Plugin):
     async def start(self) -> None:
         self.config.load_and_update()
 
+    # The Matrix Authentication Service uses an internal ID number differant
+    # than the given token. This function isn't the most effecient, and may
+    # start to be a problem on larger lists of tokens, but for now it works.
+    async def _get_token_id(self, base_url, access_token, tokenID):
+        url = base_url + '/v1/user-registration-tokens'
+        payload = '{}'
+        headers = {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer {}'.format(access_token)
+        }
+        url += "?page[first]=1000" # Fix for auth API only returning 10 by default -_-
+        r = await self.http.get(url, data=payload, headers=headers)
+        if (r.status != 200):
+            return False, "ERROR: {}".format(r.status)
+        resp = await r.json()
+        for token in resp["data"]:
+            if token["attributes"]["token"] == tokenID:
+                return True, token["id"]
+        return False, ""
+
     async def _get_token(self, base_url, access_token, tokenID):
-        url = base_url + '/v1/registration_tokens'
+        url = base_url + '/v1/user-registration-tokens'
+        if tokenID:
+            ret, realTokenID = await self._get_token_id(base_url, access_token, tokenID)
+            if ret:
+                tokenID = realTokenID
         if tokenID:
             url += "/{}".format(tokenID)
+        url += "?page[first]=1000" # Fix for auth API only returning 10 by default -_-
         payload = '{}'
         headers = {
             'content-type': 'application/json',
@@ -98,12 +128,13 @@ class TokenBot(Plugin):
 
     async def _gen_token(self, base_url, access_token, uses_allowed,
                          expiry_time):
-        url = base_url + '/v1/registration_tokens/new'
+        url = base_url + '/v1/user-registration-tokens'
         payload = {}
         if uses_allowed >= 0:
-            payload["uses_allowed"] = uses_allowed
-        if expiry_time >= 0:
-            payload["expiry_time"] = expiry_time
+            payload["usage_limit"] = uses_allowed
+        if expiry_time:
+            # DateTime Format: 2026-02-23T19:00:00Z
+            payload["expires_at"] = expiry_time.strftime("%Y-%m-%dT%H:%M:%SZ")
         headers = {
             'content-type': 'application/json',
             'Authorization': 'Bearer {}'.format(access_token)
@@ -111,21 +142,42 @@ class TokenBot(Plugin):
         r = await self.http.post(url,
                                  data=json.dumps(payload),
                                  headers=headers)
-        if (r.status != 200):
+        if (r.status != 201):
             return False, "ERROR: {}".format(r.status)
         return True, await r.json()
 
     async def _del_token(self, base_url, access_token, tokenID):
-        url = base_url + '/v1/registration_tokens/{}'.format(tokenID)
+        ret, realTokenID = await self._get_token_id(base_url, access_token, tokenID)
+        if ret:
+            tokenID = realTokenID        
+        url = base_url + '/v1/user-registration-tokens/{}/revoke'.format(tokenID)
         payload = '{}'
         headers = {
             'content-type': 'application/json',
             'Authorization': 'Bearer {}'.format(access_token)
         }
-        r = await self.http.delete(url, data=payload, headers=headers)
+        r = await self.http.post(url, data=payload, headers=headers)
         if (r.status != 200):
             return False, "ERROR: {}".format(r.status)
         return True, ""
+
+    async def _undel_token(self, base_url, access_token, tokenID):
+        ret, realTokenID = await self._get_token_id(base_url, access_token, tokenID)
+        if ret:
+            tokenID = realTokenID        
+        url = base_url + '/v1/user-registration-tokens/{}/unrevoke'.format(tokenID)
+        payload = '{}'
+        headers = {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer {}'.format(access_token)
+        }
+        r = await self.http.post(url, data=payload, headers=headers)
+        if (r.status != 200):
+            return False, "ERROR: {}".format(r.status)
+        return True, ""
+
+
+
 
     @command.new(name=lambda self: self.config["base_command"],
                  help="List available Tokens",
@@ -167,7 +219,7 @@ class TokenBot(Plugin):
             uses = self.config["default_uses_allowed"]
         if not expiry:
             expiry = self.config["default_expiry_time"]
-        expiry = expiry * 1000 + int(time.time()) * 1000
+        expiry = datetime.datetime.now() + datetime.timedelta(0,expiry)
         ret, available_token = await self._gen_token(
             self.config["admin_api"], self.config["access_token"], uses,
             expiry)
@@ -187,6 +239,19 @@ class TokenBot(Plugin):
         ret, available_token = await self._del_token(
             self.config["admin_api"], self.config["access_token"], token)
         msg = "Token deleted!"
+        if not ret:
+            msg = available_token
+        await event.reply(msg)
+
+    @token.subcommand(name="undelete", help="Undelete a Token")
+    @command.argument("token", required=True, pass_raw=True)
+    async def undelete_token(self, event: MessageEvent, token: str) -> None:
+        if not self.authenticate(event.sender):
+            await event.reply(error_msg_no_auth)
+            return
+        ret, available_token = await self._undel_token(
+            self.config["admin_api"], self.config["access_token"], token)
+        msg = "Token undeleted!"
         if not ret:
             msg = available_token
         await event.reply(msg)
